c736f1e030ff3abbdc89388ecc6cac79f3723143
Marius Wehrmann <marius.wehrmann@tvk.rwth-aachen.de>
2024-06-28 13:29:15 +0200

    Dyn-Radvd direkt in korretes Verzeichnis verschieben.

--- ffac-private-lte-wlan/files/etc/ipv6-dyn-radvd.sh
+++ ffac-private-lte-wlan/files/etc/ipv6-dyn-radvd.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
-# dny-uradvd Script nach Hotplug verschieben
-script_dir=$(dirname "$0")
-file_name="MOVE_uradvd-dyn.sh"
-destination="/etc/hotplug.d/iface/40-update-radvd"
-mv "${script_dir}/${file_name}" "${destination}"
-
-# Datei ausfuerbar machen
 chmod +x /etc/hotplug.d/iface/40-update-radvd
